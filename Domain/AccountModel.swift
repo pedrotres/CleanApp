71c5362a7a01de06ef79ee0459e084975d5d8f4d
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-08-01 09:10:33 -0300

    add AddAccount use case

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by pedro tres on 01/08/22.
+//
+
+import Foundation
+
+struct AccountModel {
+    let id: String
+    let name: String
+    let email: String
+    let password: String
+}
---
+++ Domain/AddAccount.swift
@@ -0,0 +1,19 @@
+//
+//  AddAccount.swift
+//  Domain
+//
+//  Created by pedro tres on 01/08/22.
+//
+
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAccountModel {
+    let name: String
+    let email: String
+    let password: String
+    let passwordConfirmation: String
+}
