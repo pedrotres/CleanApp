2c3aea2ab615cf1ebde1d81ed7a7f7c782a91c2f
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-09-25 19:45:43 -0300

    Add Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by pedro tres on 25/09/22.
+//
+
+import XCTest
+
+final class AlamofireAdapterTests: XCTestCase {
+
+    func test_() {
+
+    }
+}
